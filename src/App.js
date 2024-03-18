6c03d33fdbd68ee8f869b52aae3beb55fa6622b8
ashrafo12 <ec22432@qmul.ac.uk>
2024-03-18 16:34:48 +0000

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "./Nav";
+import Posts from "./Posts";
+import Detail from './Detail';
+import Create from "./Create";
+import Footer from "./Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+        <BrowserRouter>
+            <Nav/>
+            <Routes>
+                <Route path="/" element={<Posts />}></Route>
+                <Route path="/read/:id" element={<Detail />}></Route>
+                <Route path="/newblog" element={<Create />}></Route>
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    )
 }
-
 export default App;
